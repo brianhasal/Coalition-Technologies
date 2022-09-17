fa302b91c27360ae636ebb64ffc458ef0237cb85
Brian Hasal <brian.hasal@gmail.com>
2022-09-17 08:11:04 -0700

    Thumbnails link to full size images; Buttons for carousel work, but move the whole page

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const acButton = document.querySelectorAll(".btn");
 for(let i = 0; i < acButton.length; i++) {
   acButton[i].addEventListener("click", () => {
     if (acButton[i].style.backgroundColor !== "white") {
+      acButton.forEach(button => {
+        button.style.backgroundColor = "rgb(99, 109, 129)";
+      })
       acButton[i].style.backgroundColor = "white";
-    } else {
-      acButton[i].style.backgroundColor = "rgb(99, 109, 129)";
-    }
+
+    };
   })
-}
+};
+
+
+
 
 
 
